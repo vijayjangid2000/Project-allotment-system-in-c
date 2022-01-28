67ace58105cc64a7186a3fc17af62f38d6200ae0
vijay jangid <jangid84529vijay@gmail.com>
2022-01-28 11:17:01 +0530

    Vijay First edit

--- aMain.c
+++ aMain.c
@@ -2,10 +2,15 @@
 #include "auserInterface.h"
 
 int main() {
-    /* EDITING HEERE */
     startApp();
-    // Akshay here
-    // Vijay Here
+
+    // for loop by vijay
+    for (int i = 0; i < 20; ++i) {
+        printf("Helpp");
+        int a = 20;
+        a = a++;
+        a = 30 - a;
+    }
 }
 
 
